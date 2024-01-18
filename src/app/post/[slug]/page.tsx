24fce5114608c8eae204233a51f0a02113039bb1
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-01-19 02:48:53 +0900

    feat: apply .md libraries

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,14 +2,43 @@ import { readdir } from "fs/promises";
 
 import getPost from "@/app/lib/utils/getPost";
 
+import { MDXRemote } from "next-mdx-remote/rsc";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
+import remarkToc from "remark-toc";
+
 const Post = async ({ params }: { params: { slug: string } }) => {
   const post = await getPost({ params });
   const { data } = post;
+  const mdxSource = post.content;
   return (
-    <>
+    <div className="prose">
       <div>{data.date}</div>
-      <div>{post.content}</div>
-    </>
+      <MDXRemote
+        source={mdxSource}
+        options={{
+          mdxOptions: {
+            useDynamicImport: true,
+            remarkPlugins: [
+              remarkGfm,
+              remarkBreaks,
+              [remarkToc, { tight: true, maxDepth: 3 }],
+            ],
+            rehypePlugins: [
+              rehypeSlug,
+              [
+                rehypePrettyCode,
+                {
+                  theme: "rose-pine",
+                },
+              ],
+            ],
+          },
+        }}
+      />
+    </div>
   );
 };
 
