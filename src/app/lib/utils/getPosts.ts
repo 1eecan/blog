19138b3043cf46ce421c995a871ec3dd76fa5695
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-01-19 02:10:28 +0900

    feat: make getPosts function
    * getting PostList from public folder

---
+++ src/app/lib/utils/getPosts.ts
@@ -0,0 +1,31 @@
+import { readdir, readFile } from "fs/promises";
+import matter from "gray-matter";
+
+type Post = {
+  slug: string;
+
+  title: string;
+  date: string;
+  spoiler: string;
+};
+
+const getPosts = async () => {
+  const entries = await readdir("./public/", { withFileTypes: true });
+  const dirs = entries
+    .filter((entry) => entry.isDirectory())
+    .map((entry) => entry.name);
+  const fileContents = await Promise.all(
+    dirs.map((dir) => readFile("./public/" + dir + "/index.md", "utf8"))
+  );
+  const posts: Post[] = dirs.map((slug, i) => {
+    const fileContent = fileContents[i];
+    const { data } = matter(fileContent);
+    return { slug, title: data.title, date: data.date, spoiler: data.spoiler };
+  });
+  posts.sort((a, b) => {
+    return Date.parse(a.date) < Date.parse(b.date) ? 1 : -1;
+  });
+  return posts;
+};
+
+export default getPosts;
