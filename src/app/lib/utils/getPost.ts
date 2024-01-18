3e30df6a16cb6118178307d9c3b7c08c7b52b5a4
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-01-19 02:10:44 +0900

    feat: make getPost function
    * getting PostInformation from public folder

---
+++ src/app/lib/utils/getPost.ts
@@ -0,0 +1,11 @@
+import { readFile } from "fs/promises";
+import matter from "gray-matter";
+
+const getPost = async ({ params }: { params: { slug: string } }) => {
+  const filename = "./public/" + params.slug + "/index.md";
+  const file = await readFile(filename, "utf8");
+  const { content, data } = matter(file);
+  return { content, data };
+};
+
+export default getPost;
