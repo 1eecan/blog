1f82ef73459b474d7f54df69f03ecae13a2579d2
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-01-20 16:11:39 +0900

    fix: darkmode flickering
    * use cookie

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { cookies } from "next/headers";
 
 import Header from "../ui/components/Header/Header";
 const inter = Inter({ subsets: ["latin"] });
@@ -15,12 +16,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  function darkMode() {
+    const cookieStore = cookies();
+    return cookieStore.get("theme")?.value === "dark" ? "dark" : undefined;
+  }
   return (
-    <html lang="ko">
+    <html lang="ko" className={darkMode()}>
       <body
         className={`${inter.className} antialiased dark:bg-[#1a1a1a] dark:text-[#dddddd]`}
       >
-        <Header />
+        <Header theme={darkMode()} />
         <div className="max-w-[800px] mx-auto">{children}</div>
       </body>
     </html>
