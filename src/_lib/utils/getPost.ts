39cc20de9210adbc38bf065a4e93c64c222792dd
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-02-04 21:13:34 +0900

    chore: fix next.config.js

--- src/_lib/utils/getPost.ts
+++ src/_lib/utils/getPost.ts
@@ -1,10 +1,8 @@
 import { readFile } from "fs/promises";
 import matter from "gray-matter";
-import path from "path";
 
 const getPost = async ({ params }: { params: { slug: string } }) => {
-  const target = path.join(process.cwd(), "public/");
-  const filename = target + params.slug + "/index.md";
+  const filename = "./public/" + params.slug + "/index.md";
   const file = await readFile(filename, "utf8");
   const { content, data } = matter(file);
   return { content, data };
