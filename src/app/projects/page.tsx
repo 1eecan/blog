2c160b0adcaa73c0437ffad0429df33a6074fa9a
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-02-10 01:36:44 +0900

    feat: add title in each page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,11 @@
 const Projects = () => {
   return (
     <>
-      <div>Projects</div>
+      <header>
+        <h1 className="text-4xl font-bold">Project</h1>
+      </header>
+      <hr className="my-5" />
+      <section className="my-12 flex flex-col  gap-2 items-center"></section>
     </>
   );
 };
