cf03c1d9a8241bcf01a5173c27863cd1b2105be6
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-02-14 14:44:12 +0900

    chore: fix typo

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -9,7 +9,7 @@ const SeriesListPage = () => {
         <h1 className="text-4xl font-bold">Series</h1>
       </header>
       <hr className="my-5" />
-      <section className="my-12 flex flex-col  gap-2">
+      <section className="my-12 flex flex-col gap-2">
         {seriesList.map((seriesItem) => (
           <Link key={seriesItem} href={`/series/${seriesItem}/`}>
             <div>{seriesItem}</div>
